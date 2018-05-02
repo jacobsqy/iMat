0eacd9bb62764687688173823d50e0432788c8e1
Murat Kan <muratkan@Murats-MacBook-Pro.local>
2018-05-02 10:52:15 +0200

    lagt till de missade start filer i projektet

---
+++ src/App/AppMain.java
@@ -0,0 +1,26 @@
+package App;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.util.ResourceBundle;
+
+public class AppMain extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("App.fxml"));
+        primaryStage.setTitle("Hello World");
+        Scene scene = new Scene(root, 500, 500);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
