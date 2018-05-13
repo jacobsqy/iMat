636edcb5262db45b70b6d95def86cfb4d3d37f49
jacobsqy <jacobsqy@zenbook>
2018-05-14 00:17:24 +0200

    connected the new frontend to the backend

--- src/App/ShoppingCartView.java
+++ src/App/ShoppingCartView.java
@@ -10,14 +10,16 @@ public class ShoppingCartView extends AnchorPane {
     @FXML private Label totalPriceLabel;
 
     @FXML private void continueToShopButtonPressed() {
-
+        //TODO
+        // change the current view to the previous one
     }
 
     @FXML private void toPaymentButtonPressed() {
-
+        // TODO
+        // change the current viwe to the payment view
     }
 
     @FXML private void emptyShoppingCartButtonPressed() {
-
+        BackendController.emptyShoppingCart();
     }
 }
